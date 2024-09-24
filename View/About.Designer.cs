7a20e1d67aab4faeb3bf613aa10c81b0402b0bb7
Matvei Pavlov <matvei.pavlov@microlight.fr>
2024-09-24 12:56:39 +0200

    feat: Add F1 About menu
    + contains version, mailto support and bug report pre-built mail

---
+++ View/About.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace ML3DInstaller.View
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button2 = new Button();
+            label2 = new Label();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            lblcompany = new Label();
+            pictureBox1 = new PictureBox();
+            label7 = new Label();
+            label5 = new Label();
+            lblVersion = new Label();
+            label3 = new Label();
+            lblName = new Label();
+            label1 = new Label();
+            button1 = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Outset;
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(button2, 1, 4);
+            tableLayoutPanel1.Controls.Add(label2, 0, 4);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 1, 0);
+            tableLayoutPanel1.Controls.Add(label7, 0, 3);
+            tableLayoutPanel1.Controls.Add(label5, 0, 0);
+            tableLayoutPanel1.Controls.Add(lblVersion, 1, 2);
+            tableLayoutPanel1.Controls.Add(label3, 0, 2);
+            tableLayoutPanel1.Controls.Add(lblName, 1, 1);
+            tableLayoutPanel1.Controls.Add(label1, 0, 1);
+            tableLayoutPanel1.Controls.Add(button1, 1, 3);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(282, 164);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.Cursor = Cursors.Hand;
+            button2.Dock = DockStyle.Top;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.Location = new Point(107, 130);
+            button2.Name = "button2";
+            button2.Size = new Size(170, 31);
+            button2.TabIndex = 14;
+            button2.Text = "Send Mail Report";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Fill;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(5, 127);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 37);
+            label2.TabIndex = 13;
+            label2.Text = "Report Bug";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel2.Controls.Add(lblcompany, 0, 0);
+            tableLayoutPanel2.Controls.Add(pictureBox1, 1, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(104, 2);
+            tableLayoutPanel2.Margin = new Padding(0);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.Size = new Size(176, 50);
+            tableLayoutPanel2.TabIndex = 11;
+            // 
+            // lblcompany
+            // 
+            lblcompany.AutoSize = true;
+            lblcompany.Dock = DockStyle.Fill;
+            lblcompany.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblcompany.Location = new Point(3, 0);
+            lblcompany.Name = "lblcompany";
+            lblcompany.Size = new Size(93, 50);
+            lblcompany.TabIndex = 11;
+            lblcompany.Text = "Microlight 3D";
+            lblcompany.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(99, 0);
+            pictureBox1.Margin = new Padding(0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(77, 50);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Dock = DockStyle.Fill;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(5, 88);
+            label7.Name = "label7";
+            label7.Size = new Size(94, 37);
+            label7.TabIndex = 6;
+            label7.Text = "Contact Support";
+            label7.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Dock = DockStyle.Fill;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(5, 2);
+            label5.Name = "label5";
+            label5.Size = new Size(94, 50);
+            label5.TabIndex = 4;
+            label5.Text = "Company";
+            label5.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // lblVersion
+            // 
+            lblVersion.AutoSize = true;
+            lblVersion.Dock = DockStyle.Top;
+            lblVersion.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblVersion.Location = new Point(107, 71);
+            lblVersion.Name = "lblVersion";
+            lblVersion.Size = new Size(170, 15);
+            lblVersion.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Top;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(5, 71);
+            label3.Name = "label3";
+            label3.Size = new Size(94, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Version";
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Dock = DockStyle.Top;
+            lblName.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblName.Location = new Point(107, 54);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(170, 15);
+            lblName.TabIndex = 1;
+            lblName.Text = "ML3D Installer";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Top;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(5, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Name";
+            // 
+            // button1
+            // 
+            button1.Cursor = Cursors.Hand;
+            button1.Dock = DockStyle.Top;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.Location = new Point(107, 91);
+            button1.Name = "button1";
+            button1.Size = new Size(170, 31);
+            button1.TabIndex = 12;
+            button1.Text = "mailto:support@microlight.fr";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // About
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(282, 164);
+            Controls.Add(tableLayoutPanel1);
+            MinimumSize = new Size(298, 203);
+            Name = "About";
+            Text = "About";
+            Load += About_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
+        private Label label7;
+        private Label label5;
+        private Label lblVersion;
+        private Label label3;
+        private Label lblName;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Label lblcompany;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Label label2;
+    }
+}
---
+++ View/About.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ML3DInstaller.View
+{
+    public partial class About : Form
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        public void SetVersion(string version)
+        {
+            this.lblVersion.Text = version;
+        }
+
+        private void About_Load(object sender, EventArgs e)
+        {
+            if (Owner != null)
+            {
+                Location = new Point(Owner.Location.X + Owner.Width / 2 - Width / 2, Owner.Location.Y + Owner.Height / 2 - Height / 2);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendMail(true);
+        }
+
+        private void SendMail(bool mailOnly)
+        {
+            string mailTo = "mailto:support@microlight.fr";
+            if (!mailOnly)
+            {
+                string title = "[Bug Report] Issue with " + lblName.Text + " version " + lblVersion.Text;
+
+                string body = Uri.EscapeDataString(
+                    "Software name: " + lblName.Text + Environment.NewLine +
+                    "Software version: " + lblVersion.Text + Environment.NewLine +
+                    "Date of the encounter: " + DateTime.Now.ToString("dd-MM-yyyy HH:mm") + Environment.NewLine + Environment.NewLine
+                );
+
+                mailTo += $"?subject={title}&body={body}";
+            }
+            Process.Start(new ProcessStartInfo(mailTo) { UseShellExecute = true });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SendMail(false);
+        }
+    }
+}
